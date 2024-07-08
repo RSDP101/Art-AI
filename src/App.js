e2e3f2fa1165b124b5b1a66c02fd1b6ee0b51d60
Rodrigo Salgado Domingos Porto <rodrigaosdp@gmail.com>
2024-07-08 09:09:06 -0700

    style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import './App.css'; // Import your CSS file
 
 const App = () => {
   const [imageUrl, setImageUrl] = useState('');
   const [inputPrompt, setInputPrompt] = useState('');
   const [activeCategory, setActiveCategory] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleInputChange = (event) => {
     setInputPrompt(event.target.value);
@@ -11,6 +14,22 @@ const App = () => {
 
   const handleCategoryClick = (category) => {
     setActiveCategory(category);
+  };
+
+  const resizeImage = (blob) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = 512;
+        canvas.height = 512;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, 512, 512);
+        canvas.toBlob(resolve, 'image/png');
+      };
+      img.onerror = reject;
+      img.src = URL.createObjectURL(blob);
+    });
   };
 
   const queryAPI = async () => {
@@ -21,32 +40,43 @@ const App = () => {
       }
 
       let modelEndpoint = "";
+      let headers = {
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        "Content-Type": "application/json",
+      };
 
       switch (activeCategory) {
         case 'Retro':
-          modelEndpoint = "https://api-inference.huggingface.co/models/sd-dreambooth-library/retro3d";
+          modelEndpoint = "https://izmliqxjrmai0a47.us-east-1.aws.endpoints.huggingface.cloud";
+          headers = {
+            ...headers,
+            Accept: "image/png",
+          };
           break;
         case 'Cartoon':
           modelEndpoint = "https://api-inference.huggingface.co/models/sd-dreambooth-library/smiling-friends-cartoon-style";
+  
           break;
         case 'Hergé':
           modelEndpoint = "https://api-inference.huggingface.co/models/sd-dreambooth-library/herge-style";
+
           break;
         default:
           console.warn('Please select a category.');
           return;
       }
 
-      console.log(`fetching ${activeCategory}...`);
+      setLoading(true);
+      setError(false);
+      setImageUrl('');
+
+      console.log(`Fetching ${activeCategory}...`);
       const response = await fetch(
         modelEndpoint,
         {
-          headers: {
-            Authorization: "Bearer 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",
-            "Content-Type": "application/json",
-          },
+          headers,
           method: "POST",
-          body: JSON.stringify({ "inputs": inputPrompt }),
+          body: JSON.stringify({ inputs: inputPrompt }),
         }
       );
 
@@ -54,52 +84,76 @@ const App = () => {
         throw new Error('Failed to fetch image');
       }
 
-      console.log(`getting ${activeCategory} blob`);
-
+      console.log(`Getting ${activeCategory} blob`);
       const blob = await response.blob();
-      console.log(`here is the ${activeCategory} blob`);
+      console.log(`Here is the ${activeCategory} blob`);
       console.log(blob); // Log the binary blob received
 
-      const imageUrl = URL.createObjectURL(blob);
+      let resizedBlob = blob;
+      if (activeCategory === 'Retro' ) {
+        resizedBlob = await resizeImage(blob);
+      }
+
+      const imageUrl = URL.createObjectURL(resizedBlob);
       setImageUrl(imageUrl);
+      console.log(`Image URL created: ${imageUrl}`);
     } catch (error) {
       console.error('Error fetching image:', error);
+      setError(true);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      <div style={{ marginBottom: '20px', display: 'flex', justifyContent: 'center' }}>
-        <button
-          onClick={() => handleCategoryClick('Retro')}
-          style={{ padding: '10px 20px', fontSize: '16px', marginRight: '10px', backgroundColor: activeCategory === 'Retro' ? 'lightblue' : 'transparent', border: '1px solid black', borderRadius: '5px' }}
-        >
-          Retro
-        </button>
-        <button
-          onClick={() => handleCategoryClick('Cartoon')}
-          style={{ padding: '10px 20px', fontSize: '16px', marginRight: '10px', backgroundColor: activeCategory === 'Cartoon' ? 'lightblue' : 'transparent', border: '1px solid black', borderRadius: '5px' }}
-        >
-          Cartoon
-        </button>
-        <button
-          onClick={() => handleCategoryClick('Hergé')}
-          style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: activeCategory === 'Hergé' ? 'lightblue' : 'transparent', border: '1px solid black', borderRadius: '5px' }}
-        >
-          Hergé
-        </button>
-      </div>
-      <div style={{ marginBottom: '20px' }}>
-        <input
-          type="text"
+    <div className="App">
+      <header className="App-header">
+        <h1>AI - ART</h1>
+      </header>
+      <div className="chat-container">
+        <div className="button-container">
+          <button
+            onClick={() => handleCategoryClick('Retro')}
+            className={`button ${activeCategory === 'Retro' ? 'active' : ''}`}
+          >
+            Retro
+          </button>
+          <button
+            onClick={() => handleCategoryClick('Cartoon')}
+            className={`button ${activeCategory === 'Cartoon' ? 'active' : ''}`}
+          >
+            Cartoon
+          </button>
+          <button
+            onClick={() => handleCategoryClick('Hergé')}
+            className={`button ${activeCategory === 'Hergé' ? 'active' : ''}`}
+          >
+            Hergé
+          </button>
+        </div>
+        <textarea
           value={inputPrompt}
           onChange={handleInputChange}
           placeholder="Enter prompt..."
-          style={{ padding: '10px', fontSize: '16px', width: '300px', textAlign: 'center' }}
+          className="input-box"
         />
+        <div className="answer-box">
+          <div className="image-container">
+            {loading ? (
+              <p>Loading...</p>
+            ) : error ? (
+              <p>Model currently unavailable (try again, or try Retro style).</p>
+            ) : (
+              imageUrl ? (
+                <img src={imageUrl} alt="Generated" className="generated-image" />
+              ) : (
+                <p>No image to display</p>
+              )
+            )}
+          </div>
+        </div>
+        <button onClick={queryAPI} className="button">Generate</button>
       </div>
-      {imageUrl && <img src={imageUrl} alt="Generated Image" style={{ maxWidth: '100%', marginBottom: '20px' }} />}
-      <button onClick={queryAPI} style={{ padding: '10px 20px', fontSize: '16px' }}>Generate</button>
     </div>
   );
 };
